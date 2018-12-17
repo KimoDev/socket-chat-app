bb16d2dfb049a7d116e4b28f8ef2d2f35171d7bd
KimoDev <kimo@pc-77-107.customer.shillings.crm.lan>
2018-12-17 00:44:47 +0000

    broadcast added

--- index.js
+++ index.js
@@ -27,6 +27,19 @@ app.get('/', function(req, res){
     });
   });
 
+  io.on('connection', function (socket) {
+    socket.broadcast.emit('user connected');
+  });
+  
+//   io.on('connection', function (socket) {
+//     io.emit('this', { will: 'be received by everyone'});
+  
+//     socket.on('private message', function (from, msg) {
+//       console.log('I received a private message by ', from, ' saying ', msg);
+//     });
+
+//   });
+
 
 
 http.listen(port, function(){
